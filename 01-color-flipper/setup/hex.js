3bdadbc4f1661601bd0917e467d2d4bde78b59d5
bobkong <bobkong96@outlook.com>
2022-10-30 14:38:41 -0700

    finish color flipper

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const colorSpan = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    // get random number between 0-3
+    let color = "#";
+    for(let i = 0; i < 6; i ++) {
+        color += hex[getRandomNumber()];
+    }
+    document.body.style.backgroundColor = color;
+    colorSpan.textContent = color;
+});
+
+function getRandomNumber() {
+    return parseInt(Math.random() * hex.length);
+}
