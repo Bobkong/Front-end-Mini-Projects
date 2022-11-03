09a15b2e9ea2b40304bddb74d0fa82642c0a29ca
bobkong <bobkong96@outlook.com>
2022-11-03 15:47:42 -0700

    change order

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
 const projects = [
+
+    {
+        id: 8,
+        name: 'Todo List',
+        image: 'images/todo.png',
+        src: 'https://bobkong.github.io/Todo-List/'
+    },
+
+    {
+        id: 6,
+        name: 'Countdown Timer',
+        image: 'images/countdown.webp',
+        src: '12-countdown-timer/setup/index.html'
+    },
+
+    {
+        id: 5,
+        name: 'Tabs',
+        image: 'images/tabs.webp',
+        src: '11-tabs/setup/index.html'
+    },
+
+    {
+        id: 4,
+        name: 'Menu',
+        image: 'images/menu.webp',
+        src: '08-menu/setup/index.html'
+    }, 
+
+
+    {
+        id: 3,
+        name: 'Navbar',
+        image: 'images/nav-bar.webp',
+        src: '04-navbar/setup/index.html'
+    }, 
+
+    {
+        id: 2,
+        name: 'Reviews',
+        image: 'images/reviews.webp',
+        src: '03-reviews/setup/index.html'
+    }, 
+
+    {
+        id: 7,
+        name: 'Lorem Ipsum',
+        image: 'images/lorem.webp',
+        src: '13-lorem-ipsum/setup/index.html'
+    },
+
     {
         id: 0,
         name: 'Color Flipper',
@@ -12,57 +63,6 @@ const projects = [
         image: 'images/counter.webp',
         src: '02-counter/setup/index.html'
     }, 
-
-    {
-        id: 2,
-        name: 'Reviews',
-        image: 'images/reviews.webp',
-        src: '03-reviews/setup/index.html'
-    }, 
-
-    {
-        id: 3,
-        name: 'Navbar',
-        image: 'images/nav-bar.webp',
-        src: '04-navbar/setup/index.html'
-    }, 
-
-    {
-        id: 4,
-        name: 'Menu',
-        image: 'images/menu.webp',
-        src: '08-menu/setup/index.html'
-    }, 
-
-    {
-        id: 5,
-        name: 'Tabs',
-        image: 'images/tabs.webp',
-        src: '11-tabs/setup/index.html'
-    },
-    
-    {
-        id: 6,
-        name: 'Countdown Timer',
-        image: 'images/countdown.webp',
-        src: '12-countdown-timer/setup/index.html'
-    },
-
-    {
-        id: 7,
-        name: 'Lorem Ipsum',
-        image: 'images/lorem.webp',
-        src: '13-lorem-ipsum/setup/index.html'
-    },
-
-    {
-        id: 8,
-        name: 'Todo List',
-        image: 'images/todo.png',
-        src: 'https://bobkong.github.io/Todo-List/'
-    },
-
-
 ]
 
 const projectsDiv = document.querySelector('.projects');
